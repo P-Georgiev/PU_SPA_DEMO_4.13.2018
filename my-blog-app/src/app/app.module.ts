1009e3e87849ae5bd7b76a45a9e94aac1303ded1
Pavlin Georgiev <g.pavlin@yahoo.com>
2018-06-07 00:36:25 +0300

    Added navbar component.

--- my-blog-app/src/app/app.module.ts
+++ my-blog-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
     FooterComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
