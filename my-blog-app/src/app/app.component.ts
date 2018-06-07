45b34a40cca945fa0c7c3a2d8137a519ea6257df
Pavlin Georgiev <g.pavlin@yahoo.com>
2018-06-07 17:11:03 +0300

    Added firebase to project.

--- my-blog-app/src/app/app.component.ts
+++ my-blog-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyC-X4FNGrAngi7sy_6RA99pFpziB4pKrBI",
+      authDomain: "my-blog-app-27b09.firebaseapp.com",
+      databaseURL: "https://my-blog-app-27b09.firebaseio.com",
+      projectId: "my-blog-app-27b09",
+      storageBucket: "my-blog-app-27b09.appspot.com",
+      messagingSenderId: "57867251549"
+    })
+  }
 }
